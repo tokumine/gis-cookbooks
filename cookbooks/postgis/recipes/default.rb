e1c62fe62169bfe2868004b5eee6a6ab6facbafb
Simon Tokumine <simon@japancentre.com>
2010-10-12 12:10:26 +0900

    updated postgis to 1.5

--- cookbooks/postgis/recipes/default.rb
+++ cookbooks/postgis/recipes/default.rb
@@ -26,6 +26,18 @@ package 'postgis'
 package 'postgresql-8.4-postgis'
 
 
+# UPDATE TO PG 1.5 and fast GEOS, GDAL AND PROJ
+bash "upgrade to postgis 1.5" do
+  user "root"  
+  code <<-EOH    
+  add-apt-repository ppa:ubuntugis/ubuntugis-unstable
+  apt-get update
+  apt-get install -y libgeos-3.2.2
+  apt-get remove -y libgeos-3.1.0 libgeos-dev
+  apt-get upgrade -y postgis
+  apt-get install -y libgeos-dev  
+  EOH
+end
 
 # BASIC GIS TEMPLATE SETUP
 bash "configure postgis" do
@@ -33,18 +45,12 @@ bash "configure postgis" do
   code <<-EOH    
   createdb  -T template0 -O postgres -U postgres -E UTF8 template_postgis
   createlang plpgsql -U postgres -d template_postgis
-  psql -d template_postgis -U postgres -f /usr/share/postgresql/8.4/contrib/postgis.sql
-  psql -d template_postgis -U postgres -f /usr/share/postgresql/8.4/contrib/spatial_ref_sys.sql
+  psql -d template_postgis -U postgres -f /usr/share/postgresql/8.4/contrib/postgis-1.5/postgis.sql
+  psql -d template_postgis -U postgres -f /usr/share/postgresql/8.4/contrib/postgis-1.5/spatial_ref_sys.sql
   ldconfig
   EOH
   only_if { `psql -U postgres -t -c "select count(*) from pg_catalog.pg_database where datname = 'template_postgis'"`.include? '0'}
 end
 
-# create non-postgres user
-# allow postgres users to connect from other AWS
-# configure memory use in a config file depending on memory useage
-
-
-
-
-
+# create non-postgres user in Cap
+# allow postgres users to connect from other AWS - in the pg_hba.conf
